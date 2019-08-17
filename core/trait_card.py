7a94ca159d7d4996a896f563793ec0e05f4b26e0
A <1239@pm.me>
2019-08-17 19:45:01 +0100

    Adding __eq__ and __hash__ methods.

--- core/trait_card.py
+++ core/trait_card.py
@@ -4,8 +4,16 @@ class TraitCard:
         self.name = name
         self.climate_effect = 0
         self.food_effect = 0
-        self.trait_effect = []
 
     def __str__(self) -> str:
         return str(self.name)
+
+    def __eq__(self, other: object) -> bool:
+        if type(self) == type(other):
+            return vars(self) == vars(other)
+        else:
+            return False
+
+    def __hash__(self) -> int:
+        return hash(tuple(vars(self).values()))
 
